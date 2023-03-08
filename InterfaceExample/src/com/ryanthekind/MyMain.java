de6cfdb8267d16fd8aec473277690ccf22958430
Ryan Burns <mr.ryan.burns@outlook.com>
2023-03-07 21:56:10 -0500

    adding new example

---
+++ InterfaceExample/src/com/ryanthekind/MyMain.java
@@ -0,0 +1,25 @@
+package com.ryanthekind;
+
+/**
+ * Description: I intend to demonstrate how Interfaces work. Unlike Inheritance which 
+ * can be understood as a "Is-a" relationship. Interfaces use the implement keyword rather than extend.
+ * Interfaces are a set of rules that define behavior.
+ * 
+ * They do NOT containi business logic, only the required method signatures.
+ * 
+ * How are they different between abstract classes? Though, they're similar, they're quite different.
+ * An Interface is a set of rules that define behavior and that's it.
+ * 
+ * An abstract class can enforce behavior and can do other class things.
+ * @author ryanb
+ *
+ */
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	} //end of main
+
+} // end of class
